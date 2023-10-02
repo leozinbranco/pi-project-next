cb92fbf2cc0fde6b502471968ee8a23e94e042ea
Leonardo Branco <leonardoab@ciandt.com>
2023-10-01 22:23:52 -0300

    Adiciona modal de suporte

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,7 +1,19 @@
+'use client'
 import styles from './upload.module.css'
-import { Card, Text, CardBody } from '@chakra-ui/react'
+import { Card, Text, CardBody, IconButton } from '@chakra-ui/react'
+import { ModalSupport } from '@/components/ModalSupport'
+import { useState } from 'react'
+import { BiSupport } from 'react-icons/bi'
+export default function UploadPage () {
+  const [visivel, setVisivel] = useState(false)
+  const handlerOnCloseModal = () => {
+    setVisivel(false)
+    console.log('Abriu modal')
+  }
+  const handlerOnOpenModal = () => {
+    setVisivel(true)
+  }
 
-export default function Home () {
   return (
     <main className={styles.main}>
       <Card>
@@ -9,6 +21,14 @@ export default function Home () {
           <Text>View a summary of all your customers over the last month.</Text>
         </CardBody>
       </Card>
+      <IconButton
+        variant='outline'
+        colorScheme='teal'
+        aria-label='Send email'
+        onClick={handlerOnOpenModal}
+        icon={<BiSupport />}
+      />
+      <ModalSupport visible={visivel} onClose={handlerOnCloseModal}/>
     </main>
   )
 }
